1d967d8eb54341274631634b7973835ba25f09f3
Sinthuja Nagalingam <sinthu.n@gmail.com>
2021-07-28 18:39:23 -0700

    added user information

--- backend/settings.py
+++ backend/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django_samesite_none.middleware.SameSiteNoneMiddleware",
+    'django_samesite_none.middleware.SameSiteNoneMiddleware', #added
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', #added
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Group
 from django.forms import TextInput, Textarea
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+from users.models import Customer, CustomerUser
 
 User = get_user_model()
 
@@ -50,5 +50,38 @@ class UserAdmin(UserAdmin):
     )
     ordering = ('email', 'category')
 
+
+class CustomerAdmin(admin.ModelAdmin):
+    fieldsets = ((('Customer Information'), {'fields': (
+        'name',
+        'created',
+        'updated'
+    )}),)
+    list_display = ['name',]
+    model = Customer
+    readonly_fields = ('created', 'updated',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class CustomerUserAdmin(admin.ModelAdmin):
+    fieldsets = ((('Information'), {'fields': (
+        'user',
+        'customer',
+        'unique_id',
+        'preferred_name',
+        'program',
+        'created',
+        'updated',
+    )}),)
+    list_display = ['user', 'customer']
+    model = CustomerUser
+    readonly_fields = ('created', 'updated',)
+    search_fields = ('user', 'customer')
+    ordering = ('user', 'customer',)
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(CustomerUser, CustomerUserAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
