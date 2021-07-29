9fa3985b9be4366886e03b17db35321847ca4075
Sinthuja Nagalingam <sinthu.n@gmail.com>
2021-07-29 09:06:43 -0700

    fix cors headers issues and security

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -18,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 ################################################################################
-# Allowed hosts
-################################################################################
-ALLOWED_HOSTS = []
-
-
-################################################################################
 # Application definition
 ################################################################################
 INSTALLED_APPS = [
@@ -34,15 +29,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # local
-    'users.apps.UsersConfig',
-    'api.apps.ApiConfig',
-
     # added
+    'django.contrib.sites',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+
+    # local
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -92,6 +88,9 @@ DATABASES = {
     }
 }
 
+# for heroku
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 ################################################################################
 # Authentication
@@ -100,7 +99,7 @@ AUTH_USER_MODEL = 'users.User'
 
 
 ################################################################################
-# Password Validation
+# Password
 ################################################################################
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -108,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 8, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -127,6 +127,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+################################################################################
+# Site information
+################################################################################
+SITE_ID = 1
 
 ################################################################################
 # Static files (CSS, JavaScript, Images)
@@ -138,3 +142,62 @@ STATIC_URL = '/static/'
 # Default primary key field type
 ################################################################################
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+################################################################################
+# Session Cookies
+################################################################################
+if ENVIRONMENT == 'development':
+    SESSION_COOKIE_DOMAIN = 'localhost'
+else:
+    SESSION_COOKIE_DOMAIN = '.tiltaccess.com'
+SESSION_COOKIE_SAMESITE = 'lax'
+
+################################################################################
+# Security
+################################################################################
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://tiltaccess.com",
+    "https://www.tiltaccess.com",
+    "https://tiltstaging.dev"
+]
+CSRF_TRUSTED_ORIGINS = [
+    "tiltaccess.com",
+    "www.tiltaccess.com",
+    "tiltstaging.dev",
+]
+
+# security for development
+if ENVIRONMENT == 'development':
+    ALLOWED_HOSTS = [
+        '0.0.0.0',
+        '127.0.0.1',
+        'localhost',
+        '.amazonaws.com',
+        '.elasticbeanstalk.com',
+        '.tiltstaging.dev'
+    ]
+    CORS_ORIGIN_ALLOW_ALL = True
+
+# security for production
+elif ENVIRONMENT == 'production':
+    ALLOWED_HOSTS = [
+        '0.0.0.0',
+        '.amazonaws.com',
+        '.elasticbeanstalk.com',
+        '.tiltaccess.com',
+        '.tiltstaging.com',
+        '.tiltstaging.dev',
+    ]
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_REFERRER_POLICY = 'same-origin'
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
